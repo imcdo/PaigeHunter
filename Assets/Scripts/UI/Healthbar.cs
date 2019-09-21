0c8702be86b228c32075fc19b60900224a9f92a8
ilm353 <ian_mcdonald@rocketmail.com>
2019-09-21 12:35:09 -0500

    health bar

---
+++ Assets/Scripts/UI/Healthbar.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class Healthbar : MonoBehaviour
+{
+    [SerializeField] private Health _health;
+    private float _startWidth;
+    private float _startHealth;
+    private void Start()
+    {
+        RectTransform rt = GetComponent<RectTransform>();
+        _startWidth = rt.rect.width;
+        _startHealth = _health.Value;
+        _health.HealthListener += () =>
+        {
+            rt.sizeDelta = new Vector2(_startWidth * _health.Value / _startHealth, rt.sizeDelta.y);
+        };
+    }
+}
