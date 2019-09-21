816231280c4435d0bef0a8096ff1d30dba1a4265
ilm353 <ian_mcdonald@rocketmail.com>
2019-09-21 11:41:09 -0500

    fix conversion operator

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -5,7 +5,7 @@ using System;
 
 public class Health : MonoBehaviour
 {
-   public static implicit operator float(Health h) => h._health;
+   public static implicit operator float(Health h) => h.Value;
    
    [SerializeField] private float _initHealth = 10;
    private float _health;
