e2379dd419fe432f55a3d0bc422e193d417000ec
shiguy <williamlongshi@gmail.com>
2019-09-20 22:40:39 -0500

    WE MADE MOVEMENT

---
+++ Assets/Scripts/Playermovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    [SerializeField] float _speed = 5f;
+
+    private Vector2 _prevpos;
+    private Vector2 _velocity;
+
+    void Update()
+    {
+        float vert = Input.GetAxis("Vertical");
+        float hor = Input.GetAxis("Horizontal");
+
+        Vector2 newPos = (Vector2)transform.position + new Vector2(
+            _speed * hor * Time.deltaTime,
+            _speed * vert * Time.deltaTime
+        );
+
+        _prevpos = transform.position;
+        transform.position = newPos;
+        _velocity = (newPos - _prevpos) / Time.deltaTime;
+
+        Debug.Log(_velocity);
+    }
+}
