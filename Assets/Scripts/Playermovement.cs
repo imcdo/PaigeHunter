2b4a16cdf6ee8f5516a9d2e1c9678c3fe0148145
unknown <trumanbyrd>
2019-09-21 16:21:43 -0500

    ANIMATIONSgit add -A!

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -4,12 +4,41 @@ using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
+    private Animator _myAnimator;
+    enum Directions { East, NorthEast, North, NorthWest, West, SouthWest, South, SouthEast}
     [SerializeField] float _speed = 5f;
 
     private Vector2 _prevPos;
     private Vector2 _velocity;
     public Vector2 Velocity => _velocity;
     public Vector2 Direction { get; private set; }
+
+    private void Awake()
+    {
+        _myAnimator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Directions dir;
+        if (Mathf.Abs(Direction.x) > float.Epsilon && Mathf.Abs(Direction.y) <= float.Epsilon)
+            dir = (Direction.x < 0) ? Directions.West : Directions.East;
+        else if (Mathf.Abs(Direction.y) > float.Epsilon && Mathf.Abs(Direction.x) <= float.Epsilon)
+            dir = (Direction.x < 0) ? Directions.South : Directions.North;
+        else
+        {
+            if (Direction.x > 0 && Direction.y > 0)
+                dir = Directions.NorthEast;
+            else if (Direction.x > 0)
+                dir = Directions.SouthEast;
+            else if (Direction.y < 0)
+                dir = Directions.SouthWest;
+            else
+                dir = Directions.NorthWest;
+        }
+        _myAnimator?.SetInteger("Direction", (int) dir);
+        _myAnimator?.SetFloat("Speed", _velocity.magnitude);
+    }
 
     void FixedUpdate()
     {
