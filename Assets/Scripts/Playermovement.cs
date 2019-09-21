c43d6abdefc2784d2bda21b3d045096d698c1133
unknown <trumanbyrd>
2019-09-21 17:06:29 -0500

    Granny animated

--- Assets/Scripts/Playermovement.cs
+++ Assets/Scripts/Playermovement.cs
@@ -21,21 +21,37 @@ public class Playermovement : MonoBehaviour
     void Update()
     {
         Directions dir;
-        if (Mathf.Abs(Direction.x) > float.Epsilon && Mathf.Abs(Direction.y) <= float.Epsilon)
-            dir = (Direction.x < 0) ? Directions.West : Directions.East;
-        else if (Mathf.Abs(Direction.y) > float.Epsilon && Mathf.Abs(Direction.x) <= float.Epsilon)
-            dir = (Direction.x < 0) ? Directions.South : Directions.North;
-        else
+        /*      if (Mathf.Abs(Direction.x) > float.Epsilon && Mathf.Abs(Direction.y) <= float.Epsilon)
+                  dir = (Direction.x < 0) ? Directions.West : Directions.East;
+              else if (Mathf.Abs(Direction.y) > float.Epsilon && Mathf.Abs(Direction.x) <= float.Epsilon)
+                  dir = (Direction.y < 0) ? Directions.South : Directions.North;
+              else
+              {
+                  if (Direction.x > 0 && Direction.y > 0)
+                      dir = Directions.NorthEast;
+                  else if (Direction.x > 0)
+                      dir = Directions.SouthEast;
+                  else if (Direction.y < 0)
+                      dir = Directions.SouthWest;
+                  else
+                      dir = Directions.NorthWest;
+              } */
+        float x = Direction.x;
+        float y = Direction.y;
+        if (Mathf.Abs(x) > Mathf.Abs(y))
         {
-            if (Direction.x > 0 && Direction.y > 0)
-                dir = Directions.NorthEast;
-            else if (Direction.x > 0)
-                dir = Directions.SouthEast;
-            else if (Direction.y < 0)
-                dir = Directions.SouthWest;
+            if (x > Mathf.Epsilon)
+                dir = Directions.East;
             else
-                dir = Directions.NorthWest;
+                dir = Directions.West;
+        } else
+        {
+            if (y > Mathf.Epsilon)
+                dir = Directions.North;
+            else
+                dir = Directions.South;
         }
+
         _myAnimator?.SetInteger("Direction", (int) dir);
         _myAnimator?.SetFloat("Speed", _velocity.magnitude);
     }
