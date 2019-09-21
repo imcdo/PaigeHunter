d1060790b5df6a4bda8b7ae17d613e76fa8e1d89
ilm353 <ian_mcdonald@rocketmail.com>
2019-09-21 11:39:33 -0500

    add Health component

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Health : MonoBehaviour
+{
+   public static implicit operator float(Health h) => h._health;
+   
+   [SerializeField] private float _initHealth = 10;
+   private float _health;
+
+   public float Value
+   {
+      get { return _health; }
+      set
+      {
+         _health = value;
+         HealthListener();
+      }
+   }
+
+   public Action HealthListener = () => { };
+
+   void Awake()
+   {
+      _health = _initHealth;
+   }
+}
